9c4d7e2463b986e838a380423a335caa625de20b
whuwzp <whuwzp@outlook.com>
2018-07-12 22:13:36 +0800

    md-init

--- zhenai/paser/ManPaser.go
+++
@@ -1,9 +0,0 @@
-package paser
-
-import (
-	"github.com/whuwzp/papapa/deffer"
-)
-
-func ManPaser(all []byte) deffer.PaserResult {
-	return deffer.PaserResult{}
-}
--- zhenai/paser/MatchString.go
+++
@@ -1,9 +0,0 @@
-package paser
-
-const CityString = `<a href="(http://city.zhenai.com/[a-z0-9]+)"[^>]*>([^<]+).*`
-const MenString = `<td><a href="(http://album.zhenai.com/u/[a-z0-9]+)"[^>]*>([^<]+).*`
-const ManString = ``
-
-const NextMen = `<a class="next-page"[^h]*href="(http://city.zhenai.com/zhumadian/[0-9]+)"[^>]*>下一页<`
-
-//<a class="next-page" href="http://city.zhenai.com/zhumadian/3">下一页</a>
